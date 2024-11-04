8e35613098d8e2299aa60e9677e1f61a49082f9f
Luís Felipe Melo <luisfelipemelo56@gmail.com>
2024-11-04 20:01:34 -0300

    initial commit

---
+++ src/one_digitalinnovation_gof/Test.java
@@ -0,0 +1,57 @@
+package one_digitalinnovation_gof;
+
+import one_digitalinnovation_gof.facade.Facade;
+import one_digitalinnovation_gof.singleton.SingletonEager;
+import one_digitalinnovation_gof.singleton.SingletonLazy;
+import one_digitalinnovation_gof.singleton.SingletonLazyHolder;
+import one_digitalinnovation_gof.strategy.Comportamento;
+import one_digitalinnovation_gof.strategy.ComportamentoAgressivo;
+import one_digitalinnovation_gof.strategy.ComportamentoDefensivo;
+import one_digitalinnovation_gof.strategy.ComportamentoNormal;
+import one_digitalinnovation_gof.strategy.Robo;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // Singleton
+
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+        // Strategy
+
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+        // Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Venilton", "14801788");
+    }
+
+}
---
+++ src/one_digitalinnovation_gof/singleton/SingletonLazyHolder.java
@@ -0,0 +1,16 @@
+package one_digitalinnovation_gof.singleton;
+
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder {
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia() {
+        return InstanceHolder.instancia;
+    }
+}
